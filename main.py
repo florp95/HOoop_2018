04050f63201b58fd60a984ea172747c166792445
Gab223PC01 <pinguin.com>
2019-07-16 17:35:40 -0300

    Solo llegué a modificar la clase cliente: terminé de escribir la def modificarcategorîa

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class Fila(object):
     """Clase base de fila"""
 
     def __init__(self):
-         """constructor de la clase Fila """
+        """constructor de la clase Fila """
         self.enfila= 0
         self.fila = []
 
@@ -29,8 +29,7 @@ class FilaGeneral(Fila):
 
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila no preferencial"""
-        pass
-
+        
     def atender(self):
         """Atiende al proximo cliente prederencial"""
         pass      
@@ -38,14 +37,14 @@ class FilaGeneral(Fila):
     
 
 class cliente(object):
-     """clase cliente """
+    """clase cliente """
     def __init__(self,dni):
-         """ constructor de la clase cliente """
+        """ constructor de la clase cliente """
         self.dni=dni
         self.categoria=None
     def modificarcategoria(self, categoria):
         """modifica el atributo categoria del cliente """
-        pass
+        self.categoria=categoria
   
     
 if __name__ == "__main__":
